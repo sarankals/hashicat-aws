11e955d913e8e404944dd233495e9c29d8284245
sarankals <sarankal@gmail.com>
2020-11-21 14:24:16 +0530

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -10,8 +10,10 @@ cat << EOM > /var/www/html/index.html
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to Jiyabuju's app. Replace this text with your own.
   <!-- END -->
 
   </div>

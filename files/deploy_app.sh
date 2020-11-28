f13efc21fe02473dcc9c274a3f077c5812f4bc68
sarankals <sarankal@gmail.com>
2020-11-28 11:51:12 +0530

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -9,7 +9,7 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+  <center><img src="https://cloudstrikers.wordpress.com"</img></center>
   <center><h2>Meow World!</h2></center>
   Welcome to ${PREFIX}'s app. bujujiya
   <!-- END -->
